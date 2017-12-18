f2bf2fdbb87cc3da1c1588c1c887635809d6c226
Balwant Singh <balwant.matharu@gmail.com>
2017-12-18 21:07:06 +1100

    Initial Smart Contracts created

---
+++ contracts/VelixToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.13;
+
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+
+contract VelixCoin is MintableToken {
+  string public name = "VELIX TOKEN";
+  string public symbol = "VLX";
+  uint256 public decimals = 18;
+}
