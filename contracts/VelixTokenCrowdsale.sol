cfb3f3bd102fb4bb5fdde8ab9ce88aa27df0c88f
Guangling.Hastings <hastings_gl@outlook.com>
2017-12-27 23:37:38 +1100

    Fix spanning multiple line bugs, correct typo VelixToken -> VelixCoin, now pass compilation.

--- contracts/VelixTokenCrowdsale.sol
+++ contracts/VelixTokenCrowdsale.sol
@@ -6,14 +6,18 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 contract GustavoCoinCrowdsale is Crowdsale {
 
-  function GustavoCoinCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) 
-    Crowdsale(_startTime, _endTime, _rate, _wallet) {
+  function GustavoCoinCrowdsale(
+  	uint256 _startTime, 
+  	uint256 _endTime, 
+  	uint256 _rate, 
+  	address _wallet) 
+    Crowdsale(_startTime, _endTime, _rate, _wallet) public {
   }
 
   // creates the token to be sold.
   // override this method to have crowdsale of a specific MintableToken token.
   function createTokenContract() internal returns (MintableToken) {
-    return new VelixToken();
+    return new VelixCoin();
   }
 
 }
