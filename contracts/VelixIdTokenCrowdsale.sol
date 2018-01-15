5d5681a95698eb8fbec2d037ba94ddbff2a33aa7
Balwant Singh <balwant.matharu@gmail.com>
2018-01-15 21:33:11 +1100

    WIP

--- contracts/VelixIdTokenCrowdsale.sol
+++ contracts/VelixIdTokenCrowdsale.sol
@@ -1,23 +1,107 @@
-pragma solidity 0.4.18;
+pragma solidity ^0.4.18;
 
-import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
-import "zeppelin-solidity/contracts/token/MintableToken.sol";
+import '../token/MintableToken.sol';
+import '../math/SafeMath.sol';
 
-import "./VelixToken.sol";
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale.
+ * Crowdsales have a start and end timestamps, where investors can make
+ * token purchases and the crowdsale will assign them tokens based
+ * on a token per ETH rate. Funds collected are forwarded to a wallet
+ * as they arrive.
+ */
+contract VelixIdTokenCrowdsale {
+  using SafeMath for uint256;
+
+  // The token being sold
+  MintableToken public token;
+
+  // start and end timestamps where investments are allowed (both inclusive)
+  uint256 public startTime;
+  uint256 public endTime;
+
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  uint256 public rate;
+
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
 
-contract VelixIdTokenCrowdsale is Crowdsale {
+  function VelixIdTokenCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) public {
+    require(_startTime >= now);
+    require(_endTime >= _startTime);
+    // require(_rate > 0);
+    require(_wallet != address(0));
 
-    function VelixIdTokenCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) public
-        Crowdsale(_startTime, _endTime, _rate, _wallet)
-    {
+    token = createTokenContract();
+    startTime = _startTime;
+    endTime = _endTime;
+    rate = _rate;
+    wallet = _wallet;
+  }
 
-    }
+  // creates the token to be sold.
+  // override this method to have crowdsale of a specific mintable token.
+  function createTokenContract() internal returns (MintableToken) {
+    return new MintableToken();
+  }
 
-    // creates the token to be sold.
-    // override this method to have crowdsale of a specific MintableToken token.
-    function createTokenContract() internal returns (MintableToken) {
-        return new VelixToken();
-    }
+
+  // fallback function can be used to buy tokens
+  function () external payable {
+    // do nothing
+    // buyTokens(msg.sender);
+  }
+
+  // low level token purchase function
+  function allocateTokens(address beneficiary, uint256 tokens) public payable {
+    require(beneficiary != address(0));
+    require(validPurchase());
+
+    // uint256 weiAmount = msg.value;
+
+    // // calculate token amount to be created
+    // uint256 tokens = weiAmount.mul(rate);
+
+    // // update state
+    // weiRaised = weiRaised.add(weiAmount);
+
+    token.mint(beneficiary, tokens);
+    TokenPurchase(msg.sender, beneficiary, 0, tokens);
+
+    forwardFunds();
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal view returns (bool) {
+    bool withinPeriod = now >= startTime && now <= endTime;
+    bool nonZeroPurchase = msg.value != 0;
+    return withinPeriod && nonZeroPurchase;
+  }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() public view returns (bool) {
+    return now > endTime;
+  }
+
 
 }
